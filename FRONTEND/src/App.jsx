befe5b66280002093a18650ad2ae6a1a88df6f6c
anagha2023 <anaghavsini@gmail.com>
2025-11-14 21:12:27 -0800

    up

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -9,12 +9,8 @@ import Login from './Components/Login'
 import Contactt from './Components/Contactt'
 import Nav from './Components/Nav'
 import Home from './Components/Home'
-<<<<<<< HEAD
 import About from './Components/About'
-
-=======
-import Admindash from './Admindash'
->>>>>>> 99c25ad4588d61ca70f11e01fd6de224a2dc78de
+import Admindash from './Components/Admindash'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,11 +23,8 @@ function App() {
         <Route path='/c' element={<Contactt/>}/>
         <Route path='/n' element={<Nav/>}/>
         <Route path='/' element={<Home/>}/>
-<<<<<<< HEAD
-        <Route path='/about' element={<About/>}/>
-=======
-        <Route path='/a' element={<Admindash/>}/>
->>>>>>> 99c25ad4588d61ca70f11e01fd6de224a2dc78de
+  <Route path='/about' element={<About/>}/>
+  <Route path='/a' element={<Admindash/>}/>
       </Routes>
     </>
   )
