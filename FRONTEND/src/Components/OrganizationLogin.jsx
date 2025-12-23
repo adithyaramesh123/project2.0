44c85e4bf6d53593434386639998fba7598e6319
anagha2005 <anaghavsini@gail.com>
2025-12-23 12:16:34 +0530

    up

--- FRONTEND/src/Components/OrganizationLogin.jsx
+++ FRONTEND/src/Components/OrganizationLogin.jsx
@@ -39,7 +39,7 @@ const OrganizationLogin = () => {
                 justifyContent: "center",
                 alignItems: "center",
                 minHeight: "100vh",
-                backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url("https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?auto=format&fit=crop&w=1920&q=80")`,
+                backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url("https://images.unsplash.com/photo-1488521787991-ed7bbaae773c?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")`,
                 backgroundSize: "cover",
                 backgroundPosition: "center",
                 fontFamily: "'Poppins', sans-serif",
